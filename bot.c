8a685b45e91d69809bb358ac35ac084124668bf4
Cody Harrington <thecodeman66@hotmail.com>
2014-01-08 08:35:03 +1300

    Added buffer parsing function to the bot

--- bot.c
+++ bot.c
@@ -7,11 +7,13 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define DATA_BUFF_SIZE 1024
 #define ADDR_BUFF_SIZE 64
 #define STRING_BUFF_SIZE 16
 #define IRC_MESSAGE_SIZE 513
+#define MSG_QUEUE_LEN 128
 
 struct IRC_CONN {
 	struct addrinfo servaddr;
@@ -22,15 +24,62 @@ struct IRC_CONN {
 	int port;
 };
 
+struct IRC_MSG {
+	char *msg_type;
+	char *msg_body;
+	int msg_len;
+};
+
+void error(char *msg) {
+	perror(msg);
+	exit(errno);
+}
+
+
+void parse_buffer(char *buffer) {
+	int index = 0;	
+	char curr = buffer[index];
+	int msg_count = 0;
+	int msg_len = 1;
+
+	while (curr != '\0') {
+		if (curr == '\n') {
+			if (index > 0 && buffer[index - 1] == '\r') {
+				char msg_str[index + 1];
+                memset(msg_str, 0, index + 1);
+				strncpy(msg_str, buffer, msg_len);
+				msg_count++;
+				msg_len = 0;
+                puts(msg_str);
+			}
+        }
+        index++;
+        msg_len++;
+        curr = buffer[index];
+	}
+}
+
 void listen_server(struct IRC_CONN *ctx) {
 	ssize_t ret;
 	char read_buf[IRC_MESSAGE_SIZE];
 	connect(ctx->sockfd, ctx->servaddr.ai_addr,
 			ctx->servaddr.ai_addrlen);
 
-	while (ret = read(ctx->sockfd, read_buf, IRC_MESSAGE_SIZE - 1)) {
-		read_buf[IRC_MESSAGE_SIZE - 1] = '\0';
-		puts(read_buf);
+	for (;;) {
+		ret = read(ctx->sockfd, read_buf, IRC_MESSAGE_SIZE - 1); 
+		switch (ret) {
+			case -1:
+				error("Read error");
+                break;
+			case 0:
+				printf("Finished reading");
+				exit(0);
+                break;
+			default:
+				read_buf[ret - 1] = '\0';
+				parse_buffer(read_buf);
+                break;
+		}
 	}
 }
 
@@ -61,22 +110,19 @@ void init_context(struct IRC_CONN *ctx) {
 
 	ctx->sockfd = socket(ctx->servaddr.ai_family, 
 			ctx->servaddr.ai_socktype,
-		   	ctx->servaddr.ai_protocol);
+			ctx->servaddr.ai_protocol);
 }
 
 /* Signal handler(s) */
 static void sigint_handler(int signo) {
-	perror("sigint");
-	exit(EXIT_FAILURE);
+	error("sigint");
 }
 
 static void sigpipe_handler(int signo) {
-	perror("sigpipe");
-	exit(EXIT_FAILURE);
+	error("sigpipe");
 }
 
-int main()
-{
+int main() {
 	signal(SIGINT, sigint_handler);
 	signal(SIGPIPE, sigpipe_handler);
 	struct IRC_CONN ctx;
