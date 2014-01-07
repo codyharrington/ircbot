fc09aa9d927b8fbec19de11bbea44c4074d53736
Cody Harrington <thecodeman66@hotmail.com>
2014-01-07 23:18:10 +1300

    Got bot sending to server and reading from it

--- bot.c
+++ bot.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <signal.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define DATA_BUFF_SIZE 1024
 #define ADDR_BUFF_SIZE 64
 #define STRING_BUFF_SIZE 16
-#define IRC_MESSAGE_SIZE 512
+#define IRC_MESSAGE_SIZE 513
 
 struct IRC_CONN {
-	struct sockaddr_in servaddr;
-	char *server_addr_string;
+	struct addrinfo servaddr;
+	char *servaddr_str;
 	char *channel; 
 	char *nick;
 	int sockfd;
@@ -20,23 +23,45 @@ struct IRC_CONN {
 };
 
 void listen_server(struct IRC_CONN *ctx) {
-	/* use fgets(). A message is defined as a string, terminated by
-	 * carriage-return (\r) and line-feed(\n)
-	 */
-	while (1) {
-		continue;
+	ssize_t ret;
+	char read_buf[IRC_MESSAGE_SIZE];
+	connect(ctx->sockfd, ctx->servaddr.ai_addr,
+			ctx->servaddr.ai_addrlen);
+
+	while (ret = read(ctx->sockfd, read_buf, IRC_MESSAGE_SIZE - 1)) {
+		read_buf[IRC_MESSAGE_SIZE - 1] = '\0';
+		puts(read_buf);
 	}
+}
+
+void resolve_server(struct IRC_CONN *ctx) {
+	struct addrinfo hints;
+	struct addrinfo *res;
+	char port_str[5];
+
+	sprintf(port_str, "%d", ctx->port);
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+
+	getaddrinfo(ctx->servaddr_str, port_str, &hints, &res);
+	ctx->servaddr = *res;
 }
 
 void init_context(struct IRC_CONN *ctx) {
 	memset(&(ctx->servaddr), 0, sizeof(ctx->servaddr));
 
-	ctx->server_addr_string = "irc.rizon.net\0";
+	ctx->servaddr_str = "irc.rizon.net\0";
 	ctx->channel = "#/g/spam";
 	ctx->port = 6667;
 	ctx->nick = "chchjesus_bot\0";
 
-	ctx->servaddr.sin_family = AF_INET;
+	resolve_server(ctx);
+
+	ctx->sockfd = socket(ctx->servaddr.ai_family, 
+			ctx->servaddr.ai_socktype,
+		   	ctx->servaddr.ai_protocol);
 }
 
 /* Signal handler(s) */
@@ -54,8 +79,10 @@ int main()
 {
 	signal(SIGINT, sigint_handler);
 	signal(SIGPIPE, sigpipe_handler);
-	struct IRC_CONN *ctx;
-	init_context(ctx);
-	listen_server(ctx);
+	struct IRC_CONN ctx;
+
+	init_context(&ctx);
+	listen_server(&ctx);
+
 	return EXIT_SUCCESS;
 }
