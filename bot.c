9a26cd1ac709bcf937ee2552e8ba95bb2dd1edca
Cody Harrington <thecodeman66@hotmail.com>
2014-01-20 23:05:39 +1300

    Rearranged some of the functions to be a bit more modular. Added a free() function to gracefully release memory when the program is terminated. Made parse_read_buffer take a remainder pointer instead of returning the number of bytes left over. Made the bot context global to facilitate future external reference

--- bot.c
+++ bot.c
@@ -10,15 +10,20 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define DATA_BUFF_SIZE 1024
-#define ADDR_BUFF_SIZE 64
-#define STRING_BUFF_SIZE 16
 #define IRC_MESSAGE_SIZE 513
+#define PORT_STR_LEN 5
 #define MSG_QUEUE_LEN 128
 #define MSG_TERMINATOR "\r\n"
+#define MSG_TERMINATOR_LEN 2
+
+#define IRC_NICK "chchjesus_bot_two"
+#define IRC_SERVER "irc.rizon.net"
+#define IRC_CHANNEL "#/g/spam"
+#define IRC_PORT 6667
 
 struct IRC_CTX {
-    struct addrinfo servaddr;
+    struct sockaddr servaddr;
+    socklen_t servaddr_len;
     char *servaddr_str;
     char *channel; 
     char *nick;
@@ -34,14 +39,31 @@ struct IRC_MSG {
     int msg_len;
 };
 
+struct IRC_CTX *ctx;
+
+void free_context() {
+    close(ctx->sockfd);
+    free(ctx);
+}
+
 void error(char *msg) {
     perror(msg);
+    free_context();
     exit(errno);
 }
 
 void success(char *msg) {
     printf("%s\n", msg);
+    free_context();
     exit(EXIT_SUCCESS);
+}
+
+static void sigint_handler(int signo) {
+    error("sigint");
+}
+
+static void sigpipe_handler(int signo) {
+    error("sigpipe");
 }
 
 void write_to_socket(struct IRC_CTX *ctx, char *fmt, ...) {
@@ -69,22 +91,16 @@ void parse_irc_message(char *msg, size_t msg_len) {
     char *end_msg_ptr = NULL;
     int index = 0;
 
-    if ((end_msg_ptr = strstr(msg, " :")) != NULL) {
-        printf("%s", end_msg_ptr + 2);
-    }
+    printf("%s", msg);
 }
 
-/** 
- * Return the number of bytes left over as 
- * part of a fragmented IRC message
- */
-size_t parse_read_buffer(char *read_buf) {
+void parse_read_buffer(char *read_buf, size_t *remainder) {
     char *msg_end_ptr = NULL;
     char *read_buf_ptr = read_buf;
     size_t msg_len = 0;
 
     while ((msg_end_ptr = strstr(read_buf, MSG_TERMINATOR)) != NULL) {
-        msg_len = (msg_end_ptr + 2) - read_buf;
+        msg_len = (msg_end_ptr + MSG_TERMINATOR_LEN) - read_buf;
 
         char msg[msg_len + 1];
         memcpy(msg, read_buf, msg_len);
@@ -94,21 +110,16 @@ size_t parse_read_buffer(char *read_buf) {
         memset(msg, 0, msg_len);
         read_buf += msg_len;
     }
-    msg_len = strlen(read_buf);
-    memmove(read_buf_ptr, read_buf, msg_len);
-    return msg_len;
+    *remainder = strlen(read_buf);
+    memmove(read_buf_ptr, read_buf, *remainder);
 }
+
 
 void listen_server(struct IRC_CTX *ctx) {
     ssize_t ret = 0;
     size_t remainder = 0;
     char read_buf[IRC_MESSAGE_SIZE];
-    connect(ctx->sockfd, ctx->servaddr.ai_addr,
-            ctx->servaddr.ai_addrlen);
     
-    write_to_socket(ctx, "USER %s 0 0 :%s\r\n", ctx->nick, ctx->nick);
-    write_to_socket(ctx, "NICK %s\r\n", ctx->nick);
-
     for (;;) {
         memset(read_buf + remainder, 0, IRC_MESSAGE_SIZE - remainder);
         ret = read(ctx->sockfd, read_buf + remainder,
@@ -122,15 +133,16 @@ void listen_server(struct IRC_CTX *ctx) {
                 success("Finished reading.");
                 break;
             default:
-                remainder = parse_read_buffer(read_buf);
+                parse_read_buffer(read_buf, &remainder);
+                break;
         }
     }
 }
 
-void resolve_server(struct IRC_CTX *ctx) {
+void init_connection(struct IRC_CTX *ctx) {
     struct addrinfo hints;
     struct addrinfo *res;
-    char port_str[5] = {'\0'};
+    char port_str[PORT_STR_LEN] = {'\0'};
 
     sprintf(port_str, "%d", ctx->port);
 
@@ -139,43 +151,46 @@ void resolve_server(struct IRC_CTX *ctx) {
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     
-    /* Need to use freeaddrinfo() here to free the memory */
-    getaddrinfo(ctx->servaddr_str, port_str, &hints, &res);
-    ctx->servaddr = *res;
+    if (getaddrinfo(ctx->servaddr_str, port_str, &hints, &res) < 0) {
+        error("getaddrinfo");
+    }
+
+    memcpy(&(ctx->servaddr), res->ai_addr, res->ai_addrlen);
+    ctx->servaddr_len = res->ai_addrlen;
+    ctx->sockfd = socket(res->ai_family, res->ai_socktype,
+            res->ai_protocol);
+    if (ctx->sockfd < 0) {
+        error("socket");
+    }
+
+    freeaddrinfo(res);
+    if (connect(ctx->sockfd, &(ctx->servaddr), ctx->servaddr_len) < 0) {
+        error("connect");
+    }
 }
 
-void init_context(struct IRC_CTX *ctx) {
+void init_context() {
+    ctx = calloc(1, sizeof(struct IRC_CTX));
+
     memset(&(ctx->servaddr), 0, sizeof(ctx->servaddr));
-
-    ctx->servaddr_str = "irc.rizon.net\0";
-    ctx->channel = "#/g/spam";
-    ctx->port = 6667;
-    ctx->nick = "chchjesus_bot_two\0";
-
-    resolve_server(ctx);
-
-    ctx->sockfd = socket(ctx->servaddr.ai_family, 
-            ctx->servaddr.ai_socktype,
-            ctx->servaddr.ai_protocol);
+    ctx->servaddr_str = IRC_SERVER;
+    ctx->channel      = IRC_CHANNEL;
+    ctx->port         = IRC_PORT;
+    ctx->nick         = IRC_NICK;
 }
 
-/* Signal handler(s) */
-static void sigint_handler(int signo) {
-    error("sigint");
-}
-
-static void sigpipe_handler(int signo) {
-    error("sigpipe");
-}
 
 int main() {
     signal(SIGINT, sigint_handler);
     signal(SIGPIPE, sigpipe_handler);
 
-    struct IRC_CTX ctx;
+    init_context();
+    init_connection(ctx);
 
-    init_context(&ctx);
-    listen_server(&ctx);
+    write_to_socket(ctx, "USER %s 0 0 :%s\r\n", ctx->nick, ctx->nick);
+    write_to_socket(ctx, "NICK %s\r\n", ctx->nick);
+
+    listen_server(ctx);
 
     return EXIT_SUCCESS;
 }
